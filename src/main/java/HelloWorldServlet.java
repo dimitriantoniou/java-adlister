83d2b83d8031891c84da84927443eca1de2b11f9
Dimitri <dma2012@gmail.com>
2020-10-23 06:54:31 -0400

    bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,12 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+/*
 @WebServlet(name = "HelloServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {{
@@ -11,3 +14,19 @@ public class HelloWorldServlet extends HttpServlet {
         }
     }
 }
+*/
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String queryName;
+        if (request.getParameter("name") == null) {
+            queryName = "World";
+        } else {
+            queryName = request.getParameter("name");
+        }
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, " + queryName + "!</h1>");
+    }
+};
