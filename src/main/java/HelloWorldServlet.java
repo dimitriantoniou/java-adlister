8ecc4059ed7fd10b6ea375a745bdb793b5befec6
Dimitri <dma2012@gmail.com>
2020-10-25 13:42:11 -0400

    finished helloworld servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,6 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+    }
+
+}
+
 
 /*
 @WebServlet(name = "HelloServlet", urlPatterns = "/")
@@ -15,18 +30,3 @@ public class HelloWorldServlet extends HttpServlet {
     }
 }
 */
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String queryName;
-        if (request.getParameter("name") == null) {
-            queryName = "World";
-        } else {
-            queryName = request.getParameter("name");
-        }
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, " + queryName + "!</h1>");
-    }
-};
